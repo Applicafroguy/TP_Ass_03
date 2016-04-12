56f3e28325638850a621abea6e6fa44e8423625f
Sivuyile Magutywa <s.magutywa@applicafro.co.za>
2016-04-12 21:34:47 +0200

    first commit

---
+++ MyInterface/src/main/java/za/co/applicafro/sivuyile/domain/Students.java
@@ -0,0 +1,9 @@
+package za.co.applicafro.sivuyile.domain;
+
+/**
+ * Created by sivuyile on 4/12/16.
+ */
+public interface Students {
+    public String hello = "Hello World";
+    public void sayHello();
+}
